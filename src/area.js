23e27105ad916584fe72ba1f46a7762f1b16302e
Joshua Chan <JChan@ucvts.tec.nj.us>
2019-10-21 13:14:57 -0400

    Added comma

--- src/area.js
+++ src/area.js
@@ -7,6 +7,7 @@ let Length_in = Number(readlineSync.question("Length: "));
 let Width_mm = Width_in * Inches_to_mm;
 let Length_mm = Length_in * Inches_to_mm;
 let Area = Width_mm * Length_mm;
-Area = Area.toFixed(2);
+Area = Math.round(100*Area)/(100);
+Finalarea = Area.toLocaleString ();
 
-console.log("\nA(n) " + Width_in + "-by-" + Length_in + "-inch sheet of paper has an area of " + Area + " square millimeter(s).\n" );
+console.log("\nA(n) " + Width_in + "-by-" + Length_in + "-inch sheet of paper has an area of " + (Finalarea) + " square millimeter(s).\n" );
