0f6601d20fee26aec2403c2e8fed6d20246a4de1
Joshua Chan <JChan@ucvts.tec.nj.us>
2019-10-21 13:21:40 -0400

    Fixed spelling error

--- src/perimeter.js
+++ src/perimeter.js
@@ -11,4 +11,4 @@ let Perimeter = (2 * Length_cm) + (2 * Width_cm);
 Perimeter = Math.round(100*Perimeter)/(100);
 Finalperimeter = Perimeter.toLocaleString ();
 
-console.log("\nA(n) " + Width_in + "-by-" + Length_in + "-inch sheet of paper has a perimeter of " + (Finalperimeter) + " perimeter(s).\n");
+console.log("\nA(n) " + Width_in + "-by-" + Length_in + "-inch sheet of paper has a perimeter of " + (Finalperimeter) + " centimeters(s).\n");
